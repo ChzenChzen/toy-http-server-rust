906ab4dec9d2d627323ec8d043cab18d500d9d19
ChzenChzen <fedorovdanila@me.com>
2023-12-26 15:39:16 +0700

    Refactor imports and change RESPONSE_OK type
    The import lines have been refactored to use nested syntax for improved readability. Moreover, the `RESPONSE_OK` constant type has been changed from `&str` to `&[u8]` to eliminate the need for calling `as_bytes` method during the write operation.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
-use std::error::Error;
-use std::io::{Read, Write};
-use std::net::{TcpListener, TcpStream};
+use std::{
+    error::Error,
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
 const ADDRESS: &str = "127.0.0.1:4221";
-const RESPONSE_OK: &str = "HTTP/1.1 200 OK\r\n\r\n";
+const RESPONSE_OK: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
 
 fn main() -> Result<()> {
     println!("Listening on {ADDRESS}");
@@ -32,7 +34,7 @@ fn handle_incoming(stream: &mut TcpStream) -> Result<()> {
     string.clear();
 
     println!("sending `OK` response");
-    stream.write(RESPONSE_OK.as_bytes())?;
+    stream.write_all(RESPONSE_OK)?;
 
     Ok(())
 }
