78931431c5277901a09a2d9320a3b42542c244ce
ChzenChzen <fedorovdanila@me.com>
2023-12-26 17:27:18 +0700

    Refactor main and related functions in main.rs
    This commit includes refactoring of the main function and related methods in main.rs. Specifically, error handling has been improved with more explicit feedback, and the TcpListener now expects the action. A 'NOT_FOUND' response has been added. The parsing of the request line and routing logic are now handled in a separate function, enhancing code organization and readability.

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,56 @@
 use std::{
-    error::Error,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
 };
 
-type Result<T> = std::result::Result<T, Box<dyn Error>>;
-
 const ADDRESS: &str = "127.0.0.1:4221";
-const RESPONSE_OK: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
+const OK: &[u8] = b"HTTP/1.1 200 OK\r\n\r\n";
+const NOT_FOUND: &[u8] = b"HTTP/1.1 404 NotFound\r\n\r\n";
 
-fn main() -> Result<()> {
+fn main() {
     println!("Listening on {ADDRESS}");
-    let listener = TcpListener::bind(ADDRESS)?;
+    let listener = TcpListener::bind(ADDRESS).expect("Failed to bind");
 
-    for stream in listener.incoming() {
-        match stream {
+    for connection in listener.incoming() {
+        match connection {
             Ok(mut stream) => {
-                handle_incoming(&mut stream)?;
+                println!("Handling incoming");
+                handle_incoming(&mut stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-
-    Ok(())
 }
 
-fn handle_incoming(stream: &mut TcpStream) -> Result<()> {
-    // let mut string = String::new();
-    // stream.read_to_string(&mut string)?;
-    // println!("accepted new connection, the message is `{string}`");
-    // string.clear();
-    //
-    // println!("sending `OK` response");
-    stream.write_all(RESPONSE_OK)?;
+fn handle_incoming(stream: &mut TcpStream) {
+    let mut buffer = String::new();
+    stream
+        .read_to_string(&mut buffer)
+        .expect("Failed to read to string buffer");
 
-    Ok(())
+    match buffer.lines().next() {
+        None => panic!("The message is empty"),
+        Some(request_line) => {
+            let response = handle_request(request_line);
+            stream
+                .write_all(response)
+                .expect("Failed to write response");
+        }
+    }
+}
+
+fn handle_request(request_line: &str) -> &[u8] {
+    let [method, path, ..]: [&str; 3] = request_line
+        .split_whitespace()
+        .collect::<Vec<_>>()
+        .try_into()
+        .expect("Failed to parse request line");
+
+    if (method, path) == ("GET", "/") {
+        OK
+    } else {
+        NOT_FOUND
+    }
 }
