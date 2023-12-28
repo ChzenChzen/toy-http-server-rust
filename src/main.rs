8480afa75d55a5eab156e2b9d6061d2edeefabca
ChzenChzen <fedorovdanila@me.com>
2023-12-28 18:49:34 +0700

    Refactor request handling in main.rs
    The code related to request handling has been refactored for better readability, organization, and functionality. Handling for user agent requests has been moved to a dedicated function, 'user_agent_handler'. This results in more streamlined, easier-to-read code and provides a clearer separation of logic and function.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{BufRead, Read, Write},
+    io::{BufRead, Error, Lines, Read, Write},
+    iter::FilterMap,
     net::{TcpListener, TcpStream},
 };
 
@@ -51,22 +52,9 @@ fn get_response(buffer: &[u8]) -> String {
         .try_into()
         .expect("Failed to parse request line");
 
-    let parts: Vec<_> = path.splitn(3, '/').collect();
-    match parts.as_slice() {
-        &["", USER_AGENT_PATH] => {
-            let user_agent_line = request.nth(1).expect("Failed to get user agent line");
-
-            let user_agent = user_agent_line
-                .split_once(':')
-                .expect("Failed to split user agent line")
-                .1
-                .trim();
-
-            format!(
-                "{OK}\r\n{CONTENT_TYPE}\r\nContent-Length: {content_length}\r\n\r\n{user_agent}",
-                content_length = user_agent.len(),
-            )
-        }
+    let path_parts: Vec<_> = path.splitn(3, '/').collect();
+    match path_parts.as_slice() {
+        &["", USER_AGENT_PATH] => user_agent_handler(&mut request),
         &["", ECHO_PATH, rest] => format!(
             "{OK}\r\n{CONTENT_TYPE}\r\nContent-Length: {content_length}\r\n\r\n{rest}",
             content_length = rest.len(),
@@ -74,4 +62,19 @@ fn get_response(buffer: &[u8]) -> String {
         &["", ""] => format!("{OK}\r\n\r\n"),
         _ => format!("{NOT_FOUND}\r\n\r\n"),
     }
+}
+
+fn user_agent_handler(mut request: impl Iterator<Item = String>) -> String {
+    let user_agent_line = request.nth(1).expect("Failed to get user agent line");
+
+    let user_agent = user_agent_line
+        .split_once(':')
+        .expect("Failed to split user agent line")
+        .1
+        .trim();
+
+    format!(
+        "{OK}\r\n{CONTENT_TYPE}\r\nContent-Length: {content_length}\r\n\r\n{user_agent}",
+        content_length = user_agent.len(),
+    )
 }
