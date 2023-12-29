c832f906621ffafade9b0aa752b7d9a4176cc780
ChzenChzen <fedorovdanila@me.com>
2023-12-29 23:45:34 +0700

    Add file serving capability and refactor code
    The functionality to serve files from a specified directory has been added. This action involves parsing additional command line arguments for the flag and directory. Constants at the start of the file have been renamed and reorganized for clarity, and the response format strings have been updated to reflect these changes.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,12 @@ use std::{
 const ADDRESS: &str = "127.0.0.1:4221";
 const OK: &str = "HTTP/1.1 200 OK";
 const NOT_FOUND: &str = "HTTP/1.1 404 Not Found";
-const CONTENT_TYPE: &str = "Content-Type: text/plain";
+const TEXT_PLAIN: &str = "Content-Type: text/plain";
+const OCTET_STREAM: &str = "Content-Type: application/octet-stream";
+const CONTENT_LENGTH: &str = "Content-Length";
+const USER_AGENT_PATH: &str = "user-agent";
+const ECHO_PATH: &str = "echo";
+const FILES_PATH: &str = "files";
 
 fn main() {
     println!("Listening on {ADDRESS}");
@@ -37,9 +42,6 @@ fn handle_incoming(mut stream: TcpStream) {
         .expect("Failed to write response");
 }
 
-const USER_AGENT_PATH: &str = "user-agent";
-const ECHO_PATH: &str = "echo";
-
 fn get_response(buffer: &[u8]) -> String {
     let mut request = buffer.lines().filter_map(Result::ok);
 
@@ -48,13 +50,50 @@ fn get_response(buffer: &[u8]) -> String {
 
     let path_parts: Vec<_> = path.splitn(3, '/').collect();
     match path_parts.as_slice() {
+        &["", FILES_PATH, rest] => get_file_response(rest),
         &["", USER_AGENT_PATH] => get_user_agent_response(&mut request),
         &["", ECHO_PATH, rest] => format!(
-            "{OK}\r\n{CONTENT_TYPE}\r\nContent-Length: {content_length}\r\n\r\n{rest}",
+            "{OK}\r\n{TEXT_PLAIN}\r\n{CONTENT_LENGTH}: {content_length}\r\n\r\n{rest}",
             content_length = rest.len(),
         ),
         &["", ""] => format!("{OK}\r\n\r\n"),
         _ => format!("{NOT_FOUND}\r\n\r\n"),
+    }
+}
+
+const DIRECTORY_FLAG: &'static str = "--directory";
+
+fn get_file_response(filename: &str) -> String {
+    let (flag, directory) = parse_arguments();
+    match get_file(filename, &flag, &directory) {
+        Ok(file) => {
+            let content = String::from_utf8_lossy(&file);
+            format!(
+                "{OK}\r\n{OCTET_STREAM}\r\n{CONTENT_LENGTH}: {content_length}\r\n\r\n{content}",
+                content_length = content.len()
+            )
+        }
+        Err(_) => format!("{NOT_FOUND}\r\n\r\n"),
+    }
+}
+
+fn parse_arguments() -> (String, String) {
+    let arguments: Vec<_> = std::env::args().collect();
+    let flag = arguments.get(1).cloned().expect("Failed to get flag");
+    let directory = arguments.get(2).cloned().expect("Failed to get directory");
+    (flag, directory)
+}
+
+fn get_file(filename: &str, flag: &str, directory: &str) -> std::io::Result<Vec<u8>> {
+    match (flag, directory) {
+        (DIRECTORY_FLAG, directory) => {
+            let path = std::env::current_dir()
+                .expect("Failed to get current dir")
+                .join(directory)
+                .join(filename);
+            std::fs::read(path)
+        }
+        _ => panic!("Invalid flag for executable"),
     }
 }
 
@@ -75,7 +114,7 @@ fn get_user_agent_response(mut request: impl Iterator<Item = String>) -> String 
         .trim();
 
     format!(
-        "{OK}\r\n{CONTENT_TYPE}\r\nContent-Length: {content_length}\r\n\r\n{user_agent}",
+        "{OK}\r\n{TEXT_PLAIN}\r\n{CONTENT_LENGTH}: {content_length}\r\n\r\n{user_agent}",
         content_length = user_agent.len(),
     )
 }
